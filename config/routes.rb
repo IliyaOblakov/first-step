44610bc92c0ad0d7b73131c00b47d70fb78151d4
Iliya Oblakov <iliyaoblakov@gmail.com>
2022-06-07 14:20:59 +0100

    some changes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  root to: "users#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "pages#home"
+  devise_for :users
+  resources :bookings, only: [:new, :create, :show]
 end
