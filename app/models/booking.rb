4d7c233f523b615b468f59fdb378c79e05f832c8
Iliya Oblakov <iliyaoblakov@gmail.com>
2022-06-07 12:51:49 +0100

    models done

---
+++ app/models/booking.rb
@@ -0,0 +1,7 @@
+class Booking < ApplicationRecord
+  belongs_to :user_seeker, class_name: "User"
+  belongs_to :user_guide, class_name: "User"
+  validates :topic, presence: true
+  validates :user_guide, presence: true
+  validates :user_seeker, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_07_105823) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_07_111943) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "topic"
+    t.bigint "user_seeker_id"
+    t.bigint "user_guide_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_guide_id"], name: "index_bookings_on_user_guide_id"
+    t.index ["user_seeker_id"], name: "index_bookings_on_user_seeker_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_07_105823) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "users", column: "user_guide_id"
+  add_foreign_key "bookings", "users", column: "user_seeker_id"
 end
