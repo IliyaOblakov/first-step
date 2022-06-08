0ab9e1dfc4c8e9762c2993ccdef477e1c82cb17d
Kunal Kumar <kunalfromcbr@outlook.com>
2022-06-08 11:59:58 +0100

    do not merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   devise_for :users
-  resources :bookings, only: [:new, :create, :show, :destroy]
+  resources :bookings, only: [:new, :create, :destroy, :show]
 end
