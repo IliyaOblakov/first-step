9f032f359526f0c3a86c567cde79786096cbfe27
Iliya Oblakov <iliyaoblakov@gmail.com>
2022-06-08 11:51:03 +0100

    login routing done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  devise_for :users
-  # devise_for :users, controllers: { registrations: "registrations" }
+  # devise_for :users
+   devise_for :users, controllers: { registrations: "registrations" }
   resources :bookings, only: [:new, :create, :show]
 end
