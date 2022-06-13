e75f0ef0f664d7c458ca1af0a03750149efb2397
Iliya Oblakov <iliyaoblakov@gmail.com>
2022-06-13 17:24:42 +0100

    changes to booking pages update bookings and displaying the proper information

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_09_104114) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_13_134613) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_09_104114) do
     t.bigint "user_guide_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "price"
+    t.string "url"
+    t.string "notes"
     t.index ["user_guide_id"], name: "index_bookings_on_user_guide_id"
     t.index ["user_seeker_id"], name: "index_bookings_on_user_seeker_id"
   end
